f902db910764c6a4a6536e3d78c5004af1c5cfc0
Bhavesh Singh <Bhaveshsingh835@gmail.com>
2025-11-29 22:45:04 +0530

    completing momento pattern

--- LowLevelDesignC#/Program.cs
+++ LowLevelDesignC#/Program.cs
@@ -1,4 +1,5 @@
-﻿using LowLevelDesignC_.BehaviouralPattern.MementoPattern;
+﻿
+using LowLevelDesignC_.BehaviouralPattern.MementoPattern.GraphicEditorApp;
 using System;
 
 namespace MyApp
@@ -9,11 +10,40 @@ namespace MyApp
 
         public static void  GraphicEditorCase()
         {
+            
+            CareTaker ct = new CareTaker(); 
+            GraphicEdit ge = new GraphicEdit();
 
+            for(int i=0; i < 3; i++)
+            {
+                Console.WriteLine("Enter Your Shape Type");
+                string shape=Console.ReadLine();
+                Console.WriteLine("Enter Your Shape X cordinate");
+
+                int x = Convert.ToInt32( Console.ReadLine());
+                Console.WriteLine("Enter Your Shape Y cordinate");
+
+                int y = Convert.ToInt32( Console.ReadLine());
+                Console.WriteLine("Enter Your Shape Color");
+
+                string color = Console.ReadLine();
+                Console.WriteLine("Enter Your Shape Size");
+
+                int size = Convert.ToInt32( Console.ReadLine());
+
+                ge.Save( shape,  x,  y,  color,  size);
+                ct.SaveOptions(ge);
+
+            }
+            ct.undo(ge);
+            ct.undo(ge);
+            ge.read();  
         }
         static void Main(string[] args)
         {
-          //  Console.WriteLine("Hello World!");
+
+            GraphicEditorCase();    
+            //  Console.WriteLine("Hello World!");
 
             //TextEditor newText= new TextEditor();
             //newText.write("Hi");
@@ -28,20 +58,20 @@ namespace MyApp
             //newText.read();
             //Using momento Pattern
 
-            CareTaker ct = new CareTaker(); ;
-            TextEditor2 nt = new TextEditor2();
+            //CareTaker ct = new CareTaker(); ;
+            //TextEditor2 nt = new TextEditor2();
 
-            nt.write("Hi");
-            ct.save(nt);
+            //nt.write("Hi");
+            //ct.save(nt);
 
-            //nt.save();
-            nt.write("hey");
-            ct.save(nt);
-            nt.write("hey2");
-            ct.save(nt);
-            ct.undo(nt);
-            ct.undo(nt);
-            nt.read();
+            ////nt.save();
+            //nt.write("hey");
+            //ct.save(nt);
+            //nt.write("hey2");
+            //ct.save(nt);
+            //ct.undo(nt);
+            //ct.undo(nt);
+            //nt.read();
 
 
         }
